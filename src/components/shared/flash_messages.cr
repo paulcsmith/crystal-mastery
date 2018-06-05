c6804c9d76eb014509ca63687523b1944f1de992
Paul Smith <paulcsmith0218@gmail.com>
2018-06-04 23:18:28 -0400

    Style flash messages

--- src/components/shared/flash_messages.cr
+++ src/components/shared/flash_messages.cr
@@ -1,7 +1,7 @@
 module Shared::FlashMessages
   def render_flash
     @context.flash.each do |flash_type, flash_message|
-      div class: "flash-#{flash_type}", flow_id: "flash" do
+      div class: "flash-block -#{flash_type}", flow_id: "flash" do
         text flash_message
       end
     end
