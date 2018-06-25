1f12d08168f1e1d55a01eb73e5ef2e9d51d48137
Paul Smith <paulcsmith0218@gmail.com>
2018-06-24 20:38:19 -0400

    Split files

---
+++ src/exceptions/frame_generator.cr
@@ -0,0 +1,14 @@
+require "ecr"
+
+class Lucky::ExceptionPage::FrameGenerator
+  def self.generate_frames(message)
+    generated_frames = [] of Frame
+    if raw_frames = message.scan(/\s([^\s\:]+):(\d+)([^\n]+)/)
+      raw_frames.each_with_index do |frame, index|
+        generated_frames << Frame.new(raw_frame: frame, index: index)
+      end
+    end
+
+    generated_frames
+  end
+end
