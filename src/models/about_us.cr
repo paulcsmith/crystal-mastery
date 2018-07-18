e4da2a46b9fbc5be3841b6deab9f5573ee21eab2
Paul Smith <paulcsmith0218@gmail.com>
2018-07-18 19:54:42 -0400

    Add coming soon and stay up to date

---
+++ src/models/about_us.cr
@@ -0,0 +1,7 @@
+module AboutUs
+  extend self
+
+  def twitter_url
+    "https://twitter.com/crystalmastery"
+  end
+end
--- src/pages/episodes/index_page.cr
+++ src/pages/episodes/index_page.cr
@@ -6,8 +6,13 @@ class Episodes::IndexPage < MainLayout
   end
 
   def content
-    h1 "All Episodes", class: "page-title -indented"
+    render_episodes
+    render_coming_soon_list
+    render_stay_up_to_date
+  end
 
+  def render_episodes
+    h1 "All Episodes", class: "page-title -indented"
     @episodes.each do |episode|
       link Episodes::Show.with(episode), class: "row-block -action" do
         h2 class: "subtitle-text" do
@@ -17,6 +22,29 @@ class Episodes::IndexPage < MainLayout
         end
         span episode.title, class: "title"
       end
+    end
+  end
+
+  def render_coming_soon_list
+    h1 "Coming Soon", class: "page-title -indented -pushed-down"
+    ul class: "list-block" do
+      li "Intro to classes and modules"
+      li "Abstract modules and classes"
+      li "Handling Nil"
+      li "Preventing bugs with types"
+      li "Intro to macros"
+    end
+  end
+
+  def render_stay_up_to_date
+    h1 class: "page-title -indented -pushed-down" do
+      text "Stay in Touch for New Episodes"
+      link AboutUs.twitter_url do
+        img src: "assets/images/twitter-icon.png", class: "image"
+      end
+    end
+    link AboutUs.twitter_url, class: "row-block -action" do
+      span "Follow @crystalmastery on Twitter", class: "title"
     end
   end
 end
