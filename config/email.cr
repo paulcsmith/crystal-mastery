32d761ab6d8b078c51b39a8433c23274a2f9206a
Paul Smith <paulcsmith0218@gmail.com>
2018-07-18 22:00:59 -0400

    Use correct from address

--- config/email.cr
+++ config/email.cr
@@ -5,7 +5,7 @@ BaseEmail.configure do
     #   settings.adapter = Carbon::DevAdapter.new
     #
     # If you do need emails, get a key from SendGrid and set an ENV variable
-    # send_grid_key = send_grid_key_from_env
+    send_grid_key = send_grid_key_from_env
     settings.adapter = Carbon::SendGridAdapter.new(api_key: send_grid_key)
   else
     settings.adapter = Carbon::DevAdapter.new
--- src/emails/base_email.cr
+++ src/emails/base_email.cr
@@ -6,8 +6,12 @@ abstract class BaseEmail < Carbon::Email
   #   macro inherited
   #     from default_from
   #   end
-  #
-  #   def default_from
-  #     Carbon::Address.new("support@app.com")
-  #   end
+
+  macro inherited
+    from default_from
+  end
+
+  def default_from
+    Carbon::Address.new("paulcsmith0218+crystalmastery@gmail.com")
+  end
 end
